66fc85aab67744810c5795f942a56e3d71df4adb
Arnold Kristiansen <arnoldkristiansen@Arnolds-MBP.fritz.box>
2021-09-26 07:12:29 +0100

    push first assignment

---
+++ JS/Calculator.js
@@ -0,0 +1,77 @@
+/*
+Function which sums two numbers (a + b) from the user input and shows the result
+ */
+function addition() {
+    // Assigning two variables to the elements in the HTML file to retrieve the user input
+    var additionField1 = document.getElementById("additionField1");
+    var additionField2 = document.getElementById("additionField2");
+    var result = parseInt(additionField1.value) + parseInt(additionField2.value);
+    // Outputting the result on HTML page, to the location at the id "pTagId"
+    document.getElementById("pTagId").innerHTML = "The result is: " + result;
+    // Setting the value in the input boxes to be empty/cleared after the result is showed
+    additionField1.value = "";
+    additionField2.value = "";
+} // End of function addition
+
+/*
+Function which waits until the "Addition" button/row is pressed/clicked -
+and when pressed the function addition() is called.
+ */
+function startAdd() {
+    var additionButton = document.getElementById("additionButton");
+    additionButton.addEventListener("click", addition);
+} // End of function startAdd
+
+/*
+Function which subtracts two numbers (a - b) from the user and shows the result.
+ */
+function subtraction() {
+    // Assigning two variables to the elements in the HTML file to retrieve the user input
+    var subtractionField1 = document.getElementById("subtractionField1");
+    var subtractionField2 = document.getElementById("subtractionField2");
+    var result = parseInt(subtractionField1.value) - parseInt(subtractionField2.value);
+    // Outputting the result on HTML page, to the location at the id "pTagId"
+    document.getElementById("pTagId").innerHTML = "The result is: " + result;
+    // Setting the value in the input boxes to be empty/cleared after the result is showed
+    subtractionField1.value = "";
+    subtractionField2.value = "";
+} // End of function subtraction
+
+/*
+Function which waits until the "Subtraction" button/row is pressed/clicked -
+and when pressed the function subtraction() is called.
+ */
+function startSub() {
+    var subtractionButton = document.getElementById("subtractionButton");
+    subtractionButton.addEventListener("click", subtraction);
+} // End of function startSub
+
+/*
+Function which takes an input from the user and calculates the factorial of the input (a!) -
+and shows the result.
+ */
+function factorial() {
+    // Assigning two variables to the elements in the HTML file
+    var factorialField = document.getElementById("factorialField");
+    var factorial = 1; // variable that end up being the result which is displayed for the user
+    if (parseInt(factorialField.value) < 0) { // if user input is a negative number
+        document.getElementById("pTagId").innerHTML = "Please enter an integer, which is not a negative number!"
+    } // end if
+    else {
+        // for loop that loops as often as the number the user has inserted into the input field
+        for (var i = 1; i <= factorialField.value; i++) {
+            factorial *= i
+        } // end for loop
+        document.getElementById("pTagId").innerHTML = "The factorial of " + factorialField.value + " is " + factorial;
+        factorialField.value = "";
+    } // end else
+} // end function factorial
+
+/*
+Function which waits until the "Factorial" button/row is pressed/clicked -
+and when pressed the function addition() is called.
+ */
+function startFac() {
+    var factorialButton = document.getElementById("factorialButton");
+    factorialButton.addEventListener("click", factorial);
+} // End of function startFac
---
+++ JS/MathForKids.js
@@ -0,0 +1,196 @@
+var result = 0; // Global variable
+var question = "" // Global variable
+var correct = 0; // Global variable
+var incorrect = 0; // Global variable
+var theArray = new Array(); // Global array
+
+/*
+Function that generates two random numbers between 0 to 9 and outputs a string,
+asking user what the addition of the numbers is and calls the start() function.
+ */
+function addition() {
+    // Appends an empty string to HTML tag with id="guess"
+    document.getElementById("guess").innerHTML = "";
+    // Generates a random number between 0 to 9 and assigns to variable
+    var a1 = Math.floor(Math.random() * 10);
+    var a2 = Math.floor(Math.random() * 10);
+    // Appends a string to HTML tag with id="question" and assigns to variable
+    question = document.getElementById("question").innerHTML = "What is " + a1 + " + " + a2 + " ?"
+    result = a1 + a2; // Assigns the sum to variable
+    start(); // Calls function
+} // Function end
+
+/*
+Function that generates two random numbers between 0 to 9 and outputs a string,
+asking user what the subtraction of the numbers is and calls the start() function.
+ */
+function subtraction() {
+    // Appends an empty string to HTML tag with id="guess"
+    document.getElementById("guess").innerHTML = "";
+    // Generates a random number between 0 to 9 and assigns to variable
+    var a1 = Math.floor(Math.random() * 10);
+    var a2 = Math.floor(Math.random() * 10);
+    // Appends a string to HTML tag with id="question" and assigns to variable
+    question = document.getElementById("question").innerHTML = "What is " + a1 + " - " + a2 + " ?"
+    result = a1 - a2; // Assigns the sum to variable
+    start(); // Calls function
+} // Function end
+
+/*
+Function that generates two random numbers between 0 to 9 and outputs a string,
+asking user what the multiplication of the numbers is and calls the start() function.
+ */
+function multiplication() {
+    // Appends an empty string to HTML tag with id="guess"
+    document.getElementById("guess").innerHTML = "";
+    // Generates a random number between 0 to 9 and assigns to variable
+    var a1 = Math.floor(Math.random() * 10);
+    var a2 = Math.floor(Math.random() * 10);
+    // Appends a string to HTML tag with id="question" and assigns to variable
+    question = document.getElementById("question").innerHTML = "What is " + a1 + " x " + a2 + " ?"
+    result = a1 * a2; // Assigns the sum to variable
+    start(); // Calls function
+} // Function end
+
+/*
+Function that generates two random numbers between 0 to 9 and outputs a string,
+asking user what the multiplication of the numbers is and calls the start() function.
+ */
+function division() {
+    // Appends an empty string to HTML tag with id="guess"
+    document.getElementById("guess").innerHTML = "";
+    // Generates a random number between 0 to 9 and assigns to variable
+    var a1 = Math.floor(Math.random() * 10);
+    var a2 = Math.floor(Math.random() * 10);
+
+    if (a1 > a2) {
+        if (a2 == 0) {
+            a2 = 1;
+        }
+        // Appends a string to HTML tag with id="question" and assigns to variable
+        question = document.getElementById("question").innerHTML = "What is " + a1 * a2 + " / " + a2 + " ?"
+        result = (a1 * a2) / a2
+    }
+    else {
+        if (a1 == 0) {
+            a1 = 1;
+        }
+        // Appends a string to HTML tag with id="question" and assigns to variable
+        question = document.getElementById("question").innerHTML = "What is " + a1 * a2 + " / " + a1 + " ?"
+        result = (a1 * a2) / a1
+    }
+    start(); // Calls function
+} // Function end
+
+/*
+Function that checks if guess from user is correct, outputs the result and
+appends items to array.
+ */
+function guess() {
+    var output = ""; // Assigns empty string to variable
+    var guess = document.getElementById("inputField");
+    // Checks if user guess is correct
+    if (guess.value != "") {
+        if (guess.value == result) {
+            // Assigns string to variable
+            output = "<div>Correct &#x2611;</div>";
+            // Adds 3 items to the array
+            theArray.push(question, guess.value, output);
+            correct++;
+        }
+        // Executes if guess incorrect
+        else {
+            // Assigns string to variable
+            output = "<div>Incorrect &#x2612;</div>";
+            // Adds 3 items to the array
+            theArray.push(question, guess.value, output);
+            incorrect++;
+        }
+    }
+    else {
+        output = '<div>Please enter a number before<br>clicking on the "Check" button !</div>';
+    }
+    guess.value = ""; // Sets value to empty string
+    // Appends a string from variable to HTML tag with id="guess"
+    document.getElementById("guess").innerHTML = output;
+} // Function end
+
+/*
+Function that creates a table from the items in the array and
+outputs the table.
+ */
+function seeResults() {
+    // Assigns table and table head to variable
+    var results = "<table><caption>Overview of your guesses</caption>" +
+        "<thead style='background: black; color: white'><th>Problem</th>" +
+        "<th>Guess</th><th>Outcome</th></thead><tbody style='font-weight: bold'>";
+    // Loop through the array and adds items from array to table row and
+    // increments variable
+    for (var i = 0; i < theArray.length; i += 3) {
+        results += "<tr style='background: rgba(248, 148, 6, 1); color: black'>"
+        results += "<td>" + theArray[i] + "</td>";
+        results += "<td>" + theArray[i + 1] + "</td>";
+        results += "<td>" + theArray[i + 2] + "</td></tr>";
+    } // End of for loop
+    results += "</tbody></table>";
+    // Appends table from variable to HTML tag with id="results"
+    document.getElementById("results").innerHTML = results;
+    seeTotal();
+} // Function end
+
+/*
+Function that creates a table from the items in the array and
+outputs the table.
+ */
+function seeTotal() {
+    var rate = 100;
+
+    // If incorrect, will calculate rate, else rate is unchanged.
+    if (incorrect > 0) {
+        rate = ((correct * 100) / (correct + incorrect)).toFixed(0);
+    }
+    // Assigns table to variable
+    var total = "<table><caption>Total / Succes Rate %</caption>" +
+        "<thead style='background: black; color: white'><th>Correct &#x2611;</th>" +
+        "<th>Incorrect &#x2612;</th></thead><tbody style='font-weight: bold'>";
+    // Increment variable
+    total += "<tr style='background: rgba(248, 148, 6, 1); color: black'>"
+    total += "<td>" + correct + "</td><td>" + incorrect + "</td></tr>";
+    total += "<tr style='background: lightgray; color: black'>"
+    total += "<td>Succes Rate:</td><td>" + rate + "%</td></tr>"
+    total += "</tbody></table>";
+    // Appends table from variable to HTML tag with id="total"
+    document.getElementById("total").innerHTML = total;
+} // Function end
+
+/*
+Function that adds EventListener to button, at calls guess() function
+when clicked.
+ */
+function start() {
+    var checkButton = document.getElementById("checkButton");
+    checkButton.addEventListener("click", guess, false);
+} // Function end
+
+/*
+Function which takes in two arguments and creates table with name, image and nerd level.
+*/
+function about(name, level) {
+    // Assigns table to variable
+    var info = "<table><thead><th>" + name + "</th></thead>" +
+        "<tbody><tr><td><img src='../IMG/Nerd.png'></td></tr></tbody>" +
+        "<tfoot><td>NERD LEVEL <meter min='0' max='100' value='" + level + "'></meter> " + level + "%</td></tfoot>" +
+        "</table>"
+    // Appends table from variable to HTML tag with id="info"
+    document.getElementById("info").innerHTML += info;
+} // Function end
+
+/*
+Function that calls about() function 4 times.
+*/
+function aboutStart() {
+    about('ALEX SAMUELSEN', '89');
+    about('ARI T. JOHANNESEN', '97');
+    about('ARNOLD KRISTIANSEN', '95');
+    about('EMIL RONALDSSON', '88');
+} // Function end
